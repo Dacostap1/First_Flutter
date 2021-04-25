19daa104a251021489a2f08e69100abd72faff8f
Daniel Acosta <d4ni3L_15@hotmail.com>
2021-04-24 21:54:18 -0500

    Change primary color in themeData

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
           // or simply save your changes to "hot reload" in a Flutter IDE).
           // Notice that the counter didn't reset back to zero; the application
           // is not restarted.
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors
+              .blue, // Establece tonos para primary color, etcs, por ejemplo primaryColor es primarySwatch[500]
+          primaryColor: Colors.white,
         ),
         home: RandomWords());
   }
